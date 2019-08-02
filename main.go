b06fddbcd52bde6cf30f58dcdd4e81c82efe4093
hamzaanis <hamzaanis9514@gmail.com>
2019-08-02 22:10:46 +0500

    Building url

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
+	"net/url"
 )
 
 type Config struct {
 	URL    string `json:"url"`
-	Ports  int64  `json:"ports"`
+	Ports  string `json:"ports"`
 	APIKey string `json:"apiKey"`
 }
 
@@ -21,7 +24,34 @@ func loadConfig(configFile string) Config {
 	json.Unmarshal(cnfg, &c)
 	return c
 }
+
+func buildURL(c Config) string {
+	baseUrl, err := url.Parse(c.URL)
+	if err != nil {
+		log.Fatal("Malformed URL: ", err.Error())
+	}
+	baseUrlWithAddr := net.JoinHostPort(baseUrl.Hostname(), "8000")
+
+	// Parameters
+	params := url.Values{}
+	params.Add("apiKey", c.APIKey)
+	if baseUrl.Scheme != "" {
+		log.Println("There is scheme")
+	} else {
+		log.Println("There is no scheme")
+	}
+	var newQuery *url.URL
+	newQuery, err = url.Parse(fmt.Sprintf("%v://%v", baseUrl.Scheme, baseUrlWithAddr))
+	if err != nil {
+		log.Fatal("1 Malformed URL: ", err.Error())
+	}
+
+	newQuery.RawQuery = params.Encode()
+	return newQuery.String()
+}
 func main() {
 	c := loadConfig("config.json")
-	log.Println(c.APIKey)
+
+	url := buildURL(c)
+	log.Println(url)
 }
