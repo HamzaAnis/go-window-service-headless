92ae856cef8f564897c22c68845f697254cae37f
hamzaanis <hamzaanis9514@gmail.com>
2019-08-02 23:39:37 +0500

    Mod file and running after every one minute

---
+++ go.mod
@@ -0,0 +1,8 @@
+module server-headless
+
+require (
+	github.com/moul/http2curl v1.0.0 // indirect
+	github.com/parnurzeal/gorequest v0.2.15
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+)
--- main.go
+++ main.go
@@ -8,15 +8,33 @@ import (
 	"net"
 	"net/url"
 	"strconv"
+	"time"
+
+	"github.com/parnurzeal/gorequest"
 )
 
+// The struct to store the config variables
 type Config struct {
 	URL    string `json:"url"`
 	Ports  int64  `json:"ports"`
 	APIKey string `json:"apiKey"`
 }
 
+// To store the response of the endpoint
+type Response struct {
+	Server     string `json:"server"`
+	Port       int64  `json:"port"`
+	User       string `json:"user"`
+	Password   string `json:"password"`
+	Direction  string `json:"direction"`
+	Target     string `json:"target"`
+	TargetPort int64  `json:"targetPort"`
+	SourcePort int64  `json:"sourcePort"`
+}
+
+// This method loads reads the configuration file
 func loadConfig(configFile string) Config {
+	log.Printf("Loading config file %v\n", configFile)
 	cnfg, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		log.Fatalf("ERROR: %v\n", err)
@@ -26,33 +44,42 @@ func loadConfig(configFile string) Config {
 	return c
 }
 
+// This builds the url from the config file
 func buildURL(c Config) string {
-	baseUrl, err := url.Parse(c.URL)
+	baseURL, err := url.Parse(c.URL)
 	if err != nil {
 		log.Fatal("Malformed URL: ", err.Error())
 	}
-	baseUrlWithAddr := net.JoinHostPort(baseUrl.Hostname(), strconv.FormatInt(c.Ports, 10))
+
+	baseURLWithAddr := net.JoinHostPort(baseURL.Hostname(), strconv.FormatInt(c.Ports, 10))
 
 	// Parameters
 	params := url.Values{}
 	params.Add("apiKey", c.APIKey)
-	if baseUrl.Scheme != "" {
-		log.Println("There is scheme")
-	} else {
-		log.Println("There is no scheme")
-	}
+
 	var newQuery *url.URL
-	newQuery, err = url.Parse(fmt.Sprintf("%v://%v", baseUrl.Scheme, baseUrlWithAddr))
+	newQuery, err = url.Parse(fmt.Sprintf("%v://%v", baseURL.Scheme, baseURLWithAddr))
 	if err != nil {
-		log.Fatal("1 Malformed URL: ", err.Error())
+		log.Fatal("Malformed URL: ", err.Error())
 	}
 
 	newQuery.RawQuery = params.Encode()
 	return newQuery.String()
 }
+
+// Main function
 func main() {
 	c := loadConfig("config.json")
 
 	url := buildURL(c)
-	log.Println(url)
+	for {
+		log.Printf("Processing %v\n", url)
+
+		var getRestResponse []Response
+
+		request := gorequest.New()
+		_, body, _ := request.Get(url).EndStruct(&getRestResponse)
+		log.Printf("Body:\n%v\n", body)
+		time.Sleep(time.Minute * 1)
+	}
 }
